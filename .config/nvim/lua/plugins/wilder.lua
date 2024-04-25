49fe4603d66585ffe973b3d5be3d19dad2b4e7e0
Tocoe <josiahgleeson@yahoo.com>
2024-04-26 05:44:02 +1000

    included.

---
+++ .config/nvim/lua/plugins/wilder.lua
@@ -0,0 +1,59 @@
+-- Wilder
+local wilder = require('wilder')
+wilder.setup({modes = {':', '/', '?'}})
+
+wilder.set_option('pipeline', {
+	wilder.branch(
+		wilder.python_file_finder_pipeline({
+				file_command = function(ctx, arg)
+			    	if string.find(arg, '.') ~= nil then
+			    		return {'fd', '-tf', '-H'}
+			        else
+			        	return {'fd', '-tf'}
+			        end
+			    end,
+			    dir_command = {'fd', '-td', '-H'},
+			    filters = {'cpsm_filter'},
+			}),
+
+			wilder.substitute_pipeline({
+				pipeline = wilder.python_search_pipeline({
+					skip_cmdtype_check = 1,
+					pattern = wilder.python_fuzzy_pattern({
+					start_at_boundary = 0,
+					})
+				})
+			}),
+
+			wilder.cmdline_pipeline({
+				fuzzy = 2,
+				fuzzy_filter = wilder.lua_fzy_filter(),
+			}),
+    		{
+    		  wilder.check(function(ctx, x) return x == '' end),
+    		  wilder.history(),
+    		}
+		)
+	})
+
+wilder.setup {
+	next_key = "<C-j>",
+	previous_key = "<C-k>",
+	accept_key = "<C-l>",
+	reject_key = "<C-h>",
+	modes = { ":", "/", "?" },
+}
+
+local highlighters = {
+	wilder.lua_fzy_highlighter(),
+	wilder.pcre2_highlighter(),
+}
+
+wilder.set_option("renderer", wilder.popupmenu_renderer(
+wilder.popupmenu_border_theme {
+	border = "rounded",
+	highlights = { border = "Normal", accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#ff007c'}}),},
+	highlighter = highlighters,
+	left = { " ", wilder.popupmenu_devicons()},
+	right = { " ", wilder.popupmenu_scrollbar()},
+}))
